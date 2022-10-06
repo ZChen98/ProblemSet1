e2673eb81185a4c8b146a95f3f77401b8bbad5f0
Justin Chen <chenz249@uw.edu>
2022-10-06 14:38:51 -0700

    Modified to pass pylint

--- problems_set1_problem2.py
+++ problems_set1_problem2.py
@@ -5,19 +5,19 @@ Problem 2: Basic Operations
 import math
 
 # 1. Plug two vairables into the compound interest formula and format the answer to two decimals
-deposite_amounts = 1000
-interest_rate = 0.05
+deposite_amounts = int(input('What is the deposite amounts?'))
+interest_rate = float(input('What is the interest rate?')) / 100
 total_wealth = deposite_amounts * pow((1 + (interest_rate)), 10)
-print("Bill's total wealth is ${:.2f}".format(total_wealth))
+print(f"Bill's total wealth is ${total_wealth:.2f}")
 
 # 2. Double the deposite amount, move variables to solve the time t in compound interest formula
 total_wealth = deposite_amounts * 2
 time_needed = math.log(total_wealth, (1 + interest_rate))
-print("It takes {:.2f} years to double Bill's money".format(time_needed))
+print(f"It takes {time_needed:.2f} years to double Bill's money")
 
 # 3. Modify the values of the variables, compare the total wealth and the desired amount
-deposite_amounts = 5000
-interest_rate = 0.2
+deposite_amounts = int(input('What is the deposite amounts?'))
+interest_rate = float(input('What is the interest rate?')) / 100
 total_wealth = deposite_amounts * pow((1 + (interest_rate)), 6)
 print(total_wealth == (deposite_amounts * 2))
 
@@ -25,15 +25,16 @@ print(total_wealth == (deposite_amounts * 2))
 client_accounts = ["Bill", 1000, "Jack", 5000, "Amy", 6700, "Cindy", 5699, "Harry", 6700]
 print(client_accounts)
 
-# 5. Loop through the list and use every other value to be keys and values 
+# 5. Loop through the list and use every other value to be keys and values
 Accounts = {client_accounts[i]: client_accounts[i + 1] for i in range(0, len(client_accounts), 2)}
 print(Accounts)
 
-# 6. Loop through the dictionary using the key as the first value of the tuple and value as the second value of the tuple
-account_tuples = [(client, money) for client, money in Accounts.items()]
+# 6. Using the key as the first value of the tuple
+# and value as the second value of the tuple
+account_tuples = list(Accounts.items())
 print(account_tuples)
 
-# 7. 
+# 7.
 # List is a collection of ordered data
 # Dictionary is a collection of unordered data that is represented in key-value pairs
-# Sets are unordered collections of data 
+# Sets are unordered collections of data
