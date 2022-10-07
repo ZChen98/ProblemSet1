06ac906b7e26a8de7a91fee78b68258468c1e6af
Justin Chen <chenz249@uw.edu>
2022-10-06 20:33:05 -0700

    Fixed part 2 and 3

--- problems_set1_problem2.py
+++ problems_set1_problem2.py
@@ -5,21 +5,20 @@ Problem 2: Basic Operations
 import math
 
 # 1. Plug two vairables into the compound interest formula and format the answer to two decimals
-deposite_amounts = int(input('What is the deposite amounts?'))
+deposit_amounts = int(input('What is the deposite amounts?'))
 interest_rate = float(input('What is the interest rate?')) / 100
-total_wealth = deposite_amounts * pow((1 + (interest_rate)), 10)
+total_wealth = deposit_amounts * pow((1 + (interest_rate)), 10)
 print(f"Bill's total wealth is ${total_wealth:.2f}")
 
-# 2. Double the deposite amount, move variables to solve the time t in compound interest formula
-total_wealth = deposite_amounts * 2
-time_needed = math.log(total_wealth, (1 + interest_rate))
+# 2. Solving time t in the compound interest formula
+time_needed = math.log(2, (1 + interest_rate))
 print(f"It takes {time_needed:.2f} years to double Bill's money")
 
 # 3. Modify the values of the variables, compare the total wealth and the desired amount
-deposite_amounts = int(input('What is the deposite amounts?'))
+deposit_amounts = int(input('What is the deposite amounts?'))
 interest_rate = float(input('What is the interest rate?')) / 100
-total_wealth = deposite_amounts * pow((1 + (interest_rate)), 6)
-print(total_wealth == (deposite_amounts * 2))
+total_wealth = deposit_amounts * pow((1 + (interest_rate)), 6)
+print(total_wealth >= (deposit_amounts * 2))
 
 # 4. Created a list with names to be string value and money to be integer value
 client_accounts = ["Bill", 1000, "Jack", 5000, "Amy", 6700, "Cindy", 5699, "Harry", 6700]
